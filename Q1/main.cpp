00e70f28191e31056737c01c139b49f155f7ad5e
ElBanna532000 <mahmoudelbanna136@gmail.com>
2024-12-03 19:18:03 +0200

    Upload Stack Menu

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -7,6 +7,13 @@ class Complex{
     int img;
 
     public:
+    Complex(){
+        real=0;
+        img=0;
+    }
+    ~Complex(){
+        cout<<"Complex object has been deleted"<<endl;
+    }
     void setReal(int realData){
         real=realData;
     }
@@ -23,23 +30,40 @@ class Complex{
     }
 
     void print() {
-        if (real < 0 && img < 0) {
-            cout << real << img << "i" << endl;
-        } else if (real == 0 && img == 0) {
+
+        if (real == 0 && img == 0) {
             cout << "No Data!" << endl;
-        } else {
+        }else {
             if (real != 0) {
                 cout << real;
             }
             if (img != 0) {
                 cout << (img > 0 && real != 0 ? "+" : "") << img << "i";
             }
-            //cout << endl;
         }
+    }
+
+    void setComplex(int real, int img){
+        this->real=real;
+        this->img=img;
+    }
+
+    Complex add(Complex c){
+        Complex result;
+        result.real = this->real + c.real;
+        result.img = this->img + c.img;
+        return result;
+    }
+
+    Complex subtract(Complex c){
+        Complex result;
+        result.real = this->real - c.real;
+        result.img = this->img - c.img;
+        return result;
     }
 };
 
-Complex add(Complex a, Complex b){
+/*Complex add(Complex a, Complex b){
     Complex result;
     result.setReal(a.getReal()+b.getReal());
     result.setImg(a.getImg()+b.getImg());
@@ -51,20 +75,33 @@ Complex subtract(Complex a, Complex b){
     result.setReal(a.getReal()-b.getReal());
     result.setImg(a.getImg()-b.getImg());
     return result;
-}
+}*/
 
 int main()
 {
     Complex c1, c2, sum, diff;
 
-    c1.setReal(-10);
-    c1.setImg(-5);
-    c1.print();
+    c1.setReal(10);
+    c1.setImg(5);
+    //c1.print();
+
+    c2.setReal(4);
+    c2.setImg(3);
+
+
+    sum = c1.add(c2);
+    sum.print();
+
+    diff = c1.subtract(c2);
+
+    diff.print();
+
+
+
 
     /*c2.setReal(4);
     c2.setImg(7);
     c2.print();
-
 
     sum=add(c1,c2);
     sum.print();
